87f2daf7ffb63e6cc1a6efd7ef44d276dd6b49d6
katiethile <lethikatie@gmail.com>
2021-02-18 23:51:45 -0800

    add files to src, add html to index.html, update animes controller

--- backend/app/controllers/api/v1/animes_controller.rb
+++ backend/app/controllers/api/v1/animes_controller.rb
@@ -1,6 +1,17 @@
 class Api::V1::AnimesController < ApplicationController
     def index 
-        @animes = Anime.all 
-        render json: @animes 
+        animes = Anime.all 
+        render json: animes 
+    end 
+
+    def create 
+        ainme = anime.create(anime_params)
+        render json: anime 
+    end 
+
+    private 
+
+    def anime_params
+        params.require(:post).permit(:title, :review, :user_id, :rating)
     end 
 end
