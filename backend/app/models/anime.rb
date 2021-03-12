69b23088b214625a7ac6855f4a96d83446d8c64b
katiethile <lethikatie@gmail.com>
2021-03-12 15:51:00 -0800

    add titlecase method to anime model

--- backend/app/models/anime.rb
+++ backend/app/models/anime.rb
@@ -1,3 +1,10 @@
 class Anime < ApplicationRecord
     belongs_to :user 
+    before_validation :make_title_case     
+
+    private
+
+    def make_title_case
+    self.title = self.title.titlecase
+    end
 end
